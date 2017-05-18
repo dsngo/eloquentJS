c2c3fa9011128b7a40dd1a09a5c00d4295889197
Plean <daniel.dungngo@gmail.com>
2017-05-19 06:02:29 +0700

    Update ExerciseC0201.js

--- ExerciseC0201.js
+++ ExerciseC0201.js
@@ -1,3 +1,3 @@
-// My solution for the excercise, notice I omitted the curly brackets for readability.
+// My solution for the exercise, notice I omitted the curly brackets for readability.
 
 for (let i = '#'; i.length<8; i+='#') console.log(i);
